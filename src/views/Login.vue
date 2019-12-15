e4ac01727ec70e3d10bc31fb7d1b416f1b761c96
bbzhanshi999 <bbzhanshi999@163.com>
2019-12-15 18:29:00 +0800

    association with koa-demo

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <div>
+      <label>用户名：</label>
+      <input type="text" v-model="username"/>
+    </div>
+    <div>
+      <label>密码：</label>
+      <input type="password" v-model="password"/>
+    </div>
+    <div>
+      <input type="button" value="提交" v-on:click="submit"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      username:"",
+      password:""
+    }
+  },
+  methods:{
+    submit () {
+      this.$axios.post('/sigin',{username:this.username,password:this.password}).then((res)=>{
+        alert(JSON.stringify(res.data))
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>
+      <label>用户名：</label>
+      <input type="text" v-model="user.username" />
+    </div>
+    <div>
+      <label>密码：</label>
+      <input type="password" v-model="user.password" />
+    </div>
+    <div>
+      <label>年龄：</label>
+      <input type="number" v-model="user.age" />
+    </div>
+    <div>
+      <input type="button" value="提交" v-on:click="submit" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+        age: 0
+      }
+    };
+  },
+  methods:{
+    submit () {
+        axios.post("/signup",this.user).then((res)=>{
+            alert(JSON.stringify(res.data))
+        })
+    }
+  }
+};
+</script>
+
+<style>
+</style>
