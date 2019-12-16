22196cf93728b92b55299bd12ea1682a80cada68
bbzhanshi999 <bbzhanshi999@163.com>
2019-12-16 17:29:08 +0800

    u

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,39 +1,70 @@
-<template>
-  <div>
-    <div>
-      <label>用户名：</label>
-      <input type="text" v-model="user.username" />
-    </div>
-    <div>
-      <label>密码：</label>
-      <input type="password" v-model="user.password" />
-    </div>
-    <div>
-      <label>年龄：</label>
-      <input type="number" v-model="user.age" />
-    </div>
-    <div>
-      <input type="button" value="提交" v-on:click="submit" />
-    </div>
-  </div>
-</template>
+
 
 <script>
 export default {
+  /*  render: function(createElement) {
+    var self = this;
+    return createElement("div", [
+      createElement(
+        "div",
+        {
+          class: {
+            foo: true,
+            bar: false
+          },
+          domProps: {
+            id: "haha"
+          }
+        },
+        [
+          createElement("label", "用户名"),
+          createElement("input", {
+            domProps: {
+              value: self.user.username
+            },
+            on: {
+              input: function(event) {
+                console.log(event.target.value)
+                //self.$emit("input", event.target.value);
+                self.user.username = event.target.value
+              }
+            }
+          }),
+          createElement('h1',this.user.username)
+        ]
+      )
+    ]);
+  }, */
+  render() {
+    const inputAttrs = {
+      type: "email",
+      placeholder: "Enter your email"
+    };
+    return (
+      <div>
+        <div>
+          <label>用户名:</label>
+          <input type="text" vModel={this.user.username} />
+        </div>
+        <h1>{this.user.username}</h1>
+        <input {...{ attrs: inputAttrs }} />
+      </div>
+    );
+  },
   data() {
     return {
       user: {
-        username: "",
+        username: "fdsfdsf",
         password: "",
         age: 0
       }
     };
   },
-  methods:{
-    submit () {
-        axios.post("/signup",this.user).then((res)=>{
-            alert(JSON.stringify(res.data))
-        })
+  methods: {
+    submit() {
+      axios.post("/signup", this.user).then(res => {
+        alert(JSON.stringify(res.data));
+      });
     }
   }
 };
